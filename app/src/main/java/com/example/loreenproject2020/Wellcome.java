622f7ebca3336667b125da3cb3ecb6f1f80660d5
Owner <noran@danon.tzafonet.org.il>
2019-10-06 10:45:25 +0300

    fixed get and set

--- app/src/main/java/com/example/loreenproject2020/Wellcome.java
+++ app/src/main/java/com/example/loreenproject2020/Wellcome.java
@@ -10,7 +10,7 @@ public class Wellcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellcome);
-        //kljjkjk
+
 
     }
 }
